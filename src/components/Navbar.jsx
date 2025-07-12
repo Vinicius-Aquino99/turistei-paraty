99272164dad97ab252449d0806df34bf04eb98b3
Vinícius Aquino <vnaquino99@gmail.com>
2025-07-12 20:33:23 -0300

    navbar arrumada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ const Navbar = ({ activeCategory, setActiveCategory }) => {
   const categories = ["Todos", "Aventura", "Passeio de Barco", "Trilha"];
   
   return (
-    <nav className="flex flex-row gap-6 justify-center bg-yellow-200 fixed top-0 left-0 w-full py-4 shadow-md z-50">
+    <nav className="flex flex-row justify-center bg-yellow-200 fixed top-0 left-0 w-full py-1 shadow-md z-50">
       {categories.map(category => (
         <button
           key={category}
           onClick={() => setActiveCategory(category)}
-          className={`px-4 py-2 rounded-full transition-all ${
+          className={`px-2 py-1 rounded-full transition-all ${
             activeCategory === category 
               ? "bg-lime-700 text-yellow-200 font-bold" 
               : "hover:bg-lime-100"
